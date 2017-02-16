1611fe13142fffdb6dde42a323c2d9a5ebd70d84
iwilsonq <iwilsonq@gmail.com>
2017-02-16 15:13:08 -0800

    wire up client to apoloo

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://iwilsonq:easypass@ds145379.mlab.com:45379/lyrical-graphql';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
