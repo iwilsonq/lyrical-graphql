ad96f715697cf6b64a47059ddd72d385ff29809e
iwilsonq <iwilsonq@gmail.com>
2017-02-16 21:22:14 -0800

    can add lyrics, behold the power of gql and apollo

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import { graphql } from 'react-apollo';
 import fetchSong from '../queries/fetchSong';
+import LyricList from './LyricList';
+import LyricCreate from './LyricCreate';
 
 class SongDetail extends Component {
   render() {
@@ -12,6 +14,8 @@ class SongDetail extends Component {
       <div>
         <Link to='/'>Back</Link>
         <h3>{song.title}</h3>
+        <LyricList lyrics={song.lyrics} />
+        <LyricCreate songId={this.props.params.id} />
       </div>
     );
   }
